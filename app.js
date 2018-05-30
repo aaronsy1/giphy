4f769db7e8338a0924f4070cf1b935d76811a796
Aaron <aaronssy1@gmail.com>
2018-05-29 21:56:47 -0700

    TODO stop and animate gifs

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
-var animeArr = ['DBZ', 'Bleach', 'Neon Genesis Evangelion', 'Hunter X Hunter', 'My Hero Academia', 'FLCL', 'Toradora', 'Persona 5'];
-var URL = "https://api.giphy.com/v1/gifs/search?api_key=OghruOgDY1nrR54hUP58lxyYRYRa5UoR&q=" + title + "&limit=10&offset=0&rating=G&lang=en";
-var title;
+var animeArr = ['DBZ', 'Bleach', 'Neon Genesis Evangelion', 'Hunter X Hunter', 'My Hero Academia', 'FLCL', 'Toradora', 'Persona 5',];
 
 function createBtn() {
-    var newBtn = $("<button>").attr('class',"btn btn-outline-primary animeGif");
-    $(newBtn).attr('id',animeArr[i]);
+    var newBtn = $("<button>").attr('class', "btn btn-outline-primary animeGif");
+    $(newBtn).attr('id', animeArr[i]);
     $(newBtn).text(animeArr[i]);
     $('#gifButtons').prepend(newBtn);
 }
 
-for(var i = 0; i<animeArr.length; i++){
-   createBtn();
-    }
+function populateGif() {
+    
+        $('.animeGif').on('click', function () {
+            $('#gifPopulation').empty();
+            var title = $(this).attr('id');
+            var URL = "https://api.giphy.com/v1/gifs/search?api_key=OghruOgDY1nrR54hUP58lxyYRYRa5UoR&q=" + title + "&limit=10&offset=0&rating=G&lang=en";
+            axios.get(URL)
+                .then(function (resp) {
+                    console.log(resp.data);
+
+                    for (var j = 0; j < 10; j++) {
+                        gifDiv = $('<div>');
+                        textDiv = $('<div>');
+
+                        var gif = $('<img>').attr('src', 'https://media3.giphy.com/media/' + resp.data.data[j].id + '/giphy.gif');
+                        $(gifDiv).append(gif);
+                        $(textDiv).append("rating: " + resp.data.data[j].rating);
+                        $(gifDiv).prepend(textDiv);
+                        $('#gifPopulation').prepend(gifDiv);
+                        
+                    }
+                })
+                .catch(function (err) {
+                    console.log(err);
+                })
+        })
+
+        
+};
 
 
-$('#submitBtn').on('click',function(){
+
+$('#submitBtn').on('click', function () {
 
     var newSearch = $('#animeSearch').val().trim();
     animeArr.push(newSearch);
     console.log(animeArr);
-        createBtn();
-        i++;
+    createBtn();
+    i++;
+    populateGif();
 });
 
 
-$('.animeGif').on('click', function(){
-    title = $(this).attr('id');
-    
-    axios.get(URL)
-
-    .then(function(resp){
-        console.log(resp.data);
-
-    })
-    .catch(function(err){
-        console.log(err);
-    })
-
-
-    
-})
+for (var i = 0; i < animeArr.length; i++) {
+    createBtn();
+}
+populateGif();
